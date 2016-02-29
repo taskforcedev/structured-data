559cfb8c0b159e0c29225422a04d858d5a7a4793
David Craig <david.craig10@nhs.net>
2016-02-29 15:35:14 +0000

    Add main entity of page.

--- src/Types/SchemaOrg/NewsArticle.php
+++ src/Types/SchemaOrg/NewsArticle.php
@@ -4,14 +4,16 @@ use Taskforcedev\StructuredData\Types\SchemaTypeInterface;
 
 class NewsArticle implements SchemaTypeInterface
 {
-    public $author; // Required
-    public $datePublished; // Required
-    public $headline; // Required
-    public $image; // Required
-    public $name; // Required
-    public $publisher; // Required
-    public $dateModified; // Recommended
+    public $author; // Required.
+    public $datePublished; // Required.
+    public $headline; // Required.
+    public $image; // Required.
+    public $name; // Required.
+    public $publisher; // Required.
+    public $dateModified; // Recommended.
+    public $mainEntityOfPage; // Recommended.
 
+    // Setters
     public function setAuthor($author) { $this->author = $author; }
     public function setDatePublished($datePublished) { $this->datePublished = $datePublished; }
     public function setHeadline($headline) { $this->headline = $headline; }
@@ -19,6 +21,7 @@ class NewsArticle implements SchemaTypeInterface
     public function setName($name) { $this->name = $name; }
     public function setPublisher($publisher) { $this->publisher = $publisher; }
     public function setDateModified($dateModified) { $this->dateModified = $dateModified; }
+    public function setMainEntityOfPage($mainEntityOfPage) { $this->mainEntityOfPage = $mainEntityOfPage; }
 
     public function getJsonLd($context = true, $json_object = true)
     {
@@ -31,6 +34,15 @@ class NewsArticle implements SchemaTypeInterface
         $requiredFields = ['author', 'datePublished', 'headline', 'image', 'name', 'publisher'];
 
         foreach ($requiredFields as $field)
+        {
+            if ($this->$field !== '') {
+                $jsonLd[$field] = $this->$field;
+            }
+        }
+
+        $recommendedFields = ['dateModified'];
+
+        foreach ($recommendedFields as $field)
         {
             if ($this->$field !== '') {
                 $jsonLd[$field] = $this->$field;
