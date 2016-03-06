5798f2eecf4b22d5145d810fef42503061966c87
PrivateSniper <privatesniper@gmail.com>
2016-03-06 16:09:53 +0000

    Add SportsOrganization.

---
+++ src/Types/SchemaOrg/SportsOrganization.php
@@ -0,0 +1,54 @@
+<?php namespace Taskforcedev\StructuredData\Types\SchemaOrg;
+
+use Taskforcedev\StructuredData\Types\SchemaTypeInterface;
+
+class SportsOrganization implements SchemaTypeInterface
+{
+    public $name; // Required.
+
+    public function __construct($options = [])
+    {
+        $fields = ['name'];
+
+        foreach ($fields as $field) {
+            if (array_key_exists($field, $options)) {
+                $this->$field = $options[$field];
+            } else {
+                $this->$field = '';
+            }
+        }
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public function getJsonLd($context = true, $json_object = true)
+    {
+        $jsonLd = [
+            '@context' => 'http://schema.org',
+            '@type' => 'SportsOrganization',
+        ];
+
+        $requiredFields = ['name'];
+
+        foreach ($requiredFields as $field) {
+            if ($this->$field !== '') {
+                $jsonLd[$field] = $this->$field;
+            }
+        }
+
+        $optionalFields = [];
+
+        foreach ($optionalFields as $field) {
+            if ($this->$field !== '') {
+                $jsonLd[$field] = $this->$field;
+            }
+        }
+
+        $object = (object)$jsonLd;
+
+        return json_encode($object);
+    }
+}
