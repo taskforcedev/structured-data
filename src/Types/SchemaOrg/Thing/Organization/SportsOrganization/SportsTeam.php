93bbca91ec3a83b7a58d60635a5084bd23cbd645
PrivateSniper <privatesniper@gmail.com>
2016-03-18 23:16:49 +0000

    Add athlete using context without encoding object.

--- src/Types/SchemaOrg/Thing/Organization/SportsOrganization/SportsTeam.php
+++ src/Types/SchemaOrg/Thing/Organization/SportsOrganization/SportsTeam.php
@@ -6,22 +6,60 @@ use Taskforcedev\StructuredData\Types\SchemaOrg\Thing\Person;
 
 class SportsTeam extends SportsOrganization implements SchemaTypeInterface
 {
-    public $athletes;
+    public $athlete;
     public $coach;
 
     public function __construct($options = [])
     {
         parent::__construct($options);
-        $this->athletes = [];
+        $this->athlete = [];
     }
 
     public function addAthlete(Person $athlete)
     {
-        $this->athletes[] = $athlete;
+        $context = true;
+        $json_encode = false;
+        $this->athlete[] = $athlete->getJsonLd($context, $json_encode);
     }
 
     public function setCoach(Person $coach)
     {
         $this->coach = $coach;
+    }
+
+    public function getOptionalFields()
+    {
+        $fields = parent::getOptionalFields();
+        $fields[] = 'athlete';
+        $fields[] = 'coach';
+        return $fields;
+    }
+
+    public function getJsonLd($context = true, $json_object = true)
+    {
+        $jsonLd = [
+            '@context' => 'http://schema.org',
+            '@type' => 'SportsTeam',
+        ];
+
+        $requiredFields = [ ];
+
+        foreach ($requiredFields as $field) {
+            if ($this->$field !== '') {
+                $jsonLd[$field] = $this->$field;
+            }
+        }
+
+        $optionalFields = $this->getOptionalFields();
+
+        foreach ($optionalFields as $field) {
+            if ($this->$field !== '') {
+                $jsonLd[$field] = $this->$field;
+            }
+        }
+
+        $object = (object)$jsonLd;
+
+        return json_encode($object);
     }
 }
