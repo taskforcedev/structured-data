6707cf3e494cba4a40fd6e67528235c815c25241
PrivateSniper <privatesniper@gmail.com>
2016-03-18 21:07:22 +0000

    Add athletes and coach properties.

--- src/Types/SchemaOrg/Thing/Organization/SportsOrganization/SportsTeam.php
+++ src/Types/SchemaOrg/Thing/Organization/SportsOrganization/SportsTeam.php
@@ -2,8 +2,26 @@
 
 use Taskforcedev\StructuredData\Types\SchemaOrg\Thing\Organization\SportsOrganization;
 use Taskforcedev\StructuredData\Types\SchemaTypeInterface;
+use Taskforcedev\StructuredData\Types\SchemaOrg\Person;
 
 class SportsTeam extends SportsOrganization implements SchemaTypeInterface
 {
+    public $athletes;
+    public $coach;
 
+    public function __construct(array $options)
+    {
+        parent::__construct($options);
+        $this->athletes = [];
+    }
+
+    public function addAthlete(Person $athlete)
+    {
+        $this->athletes[] = $athlete;
+    }
+
+    public function setCoach(Person $coach)
+    {
+        $this->coach = $coach;
+    }
 }
