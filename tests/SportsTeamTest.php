98b0bc8ab7dac959750f2d00b2640164e16deee4
PrivateSniper <privatesniper@gmail.com>
2016-03-18 23:24:44 +0000

    Add assertions to sports team jsonld.

--- tests/SportsTeamTest.php
+++ tests/SportsTeamTest.php
@@ -12,10 +12,17 @@ class SportsTeamTest extends TestCase
         $person->setGivenName('David');
 
         $person2 = new Person();
-        $person2->setGivenName('David');
+        $person2->setGivenName('John');
 
         $sportsTeam->addAthlete($person);
         $sportsTeam->addAthlete($person2);
+
+        $jsonLd = $sportsTeam->getJsonLd();
+
+        $this->assertTrue(strpos($jsonLd, '"givenName":"John"') !== false);
+        $this->assertTrue(strpos($jsonLd, '"givenName":"David"') !== false);
+        $this->assertTrue(strpos($jsonLd, '"@context":"http:\/\/schema.org"') !== false);
+        $this->assertTrue(strpos($jsonLd, '"@type":"SportsTeam"') !== false);
 
         var_dump($sportsTeam->getJsonLd());
     }
