5667b05fbf4f145aec67fcd1fbdca7fc8204e7b4
PrivateSniper <privatesniper@gmail.com>
2016-03-18 21:00:06 +0000

    Create SportsTeam.

---
+++ src/Types/SchemaOrg/Thing/Organization/SportsOrganization/SportsTeam.php
@@ -0,0 +1,9 @@
+<?php namespace Taskforcedev\StructuredData\Types\SchemaOrg\Thing\Organization\SportsOrganization;
+
+use Taskforcedev\StructuredData\Types\SchemaOrg\Thing\Organization\SportsOrganization;
+use Taskforcedev\StructuredData\Types\SchemaTypeInterface;
+
+class SportsTeam extends SportsOrganization implements SchemaTypeInterface
+{
+
+}
