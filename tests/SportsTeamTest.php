61b83e6f405e252609aa1eee91c6516a649e8365
PrivateSniper <privatesniper@gmail.com>
2016-03-18 23:29:50 +0000

    Add descriptions to assertions.

--- tests/SportsTeamTest.php
+++ tests/SportsTeamTest.php
@@ -19,9 +19,9 @@ class SportsTeamTest extends TestCase
 
         $jsonLd = $sportsTeam->getJsonLd();
 
-        $this->assertTrue(strpos($jsonLd, '"givenName":"John"') !== false);
-        $this->assertTrue(strpos($jsonLd, '"givenName":"David"') !== false);
-        $this->assertTrue(strpos($jsonLd, '"@context":"http:\/\/schema.org"') !== false);
-        $this->assertTrue(strpos($jsonLd, '"@type":"SportsTeam"') !== false);
+        $this->assertTrue(strpos($jsonLd, '"givenName":"John"') !== false, 'Json has a person with givenName: John.');
+        $this->assertTrue(strpos($jsonLd, '"givenName":"David"') !== false, 'Json has a person with givenName: David.');
+        $this->assertTrue(strpos($jsonLd, '"@context":"http:\/\/schema.org"') !== false, 'Json has the context for schema.org.');
+        $this->assertTrue(strpos($jsonLd, '"@type":"SportsTeam"') !== false, 'Json has the type of SportsTeam as expected.');
     }
 }
