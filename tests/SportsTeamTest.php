cda505df10ef4e4067549ba8af8ecfd3ef1dec1a
PrivateSniper <privatesniper@gmail.com>
2016-03-18 23:18:09 +0000

    Test add athlete (person).

--- tests/SportsTeamTest.php
+++ tests/SportsTeamTest.php
@@ -1,12 +1,22 @@
 <?php namespace Taskforcedev\StructuredData\Tests;
 
 use Taskforcedev\StructuredData\Types\SchemaOrg\Thing\Organization\SportsOrganization\SportsTeam;
+use Taskforcedev\StructuredData\Types\SchemaOrg\Thing\Person;
 
 class SportsTeamTest extends TestCase
 {
-    public function testSettingAthleteThrowsAnError()
+    public function testSettingAthleteWorksIfPassedPersonInstance()
     {
         $sportsTeam = new SportsTeam();
-        var_dump($sportsTeam);
+        $person = new Person();
+        $person->setGivenName('David');
+
+        $person2 = new Person();
+        $person2->setGivenName('David');
+
+        $sportsTeam->addAthlete($person);
+        $sportsTeam->addAthlete($person2);
+
+        var_dump($sportsTeam->getJsonLd());
     }
 }
