b0f9748808fba1bd2826b1f6d06c18f8765a10b6
David Craig <david.craig10@nhs.net>
2016-04-18 10:10:40 +0100

    Create placeholder classes for VideoGame.

---
+++ src/Types/SchemaOrg/Thing/CreativeWork/SoftwareApplication/VideoGame.php
@@ -0,0 +1,47 @@
+<?php namespace Taskforcedev\StructuredData\Types\SchemaOrg\Thing\CreativeWork\SoftwareApplicatoin;
+
+use Taskforcedev\StructuredData\Types\SchemaTypeInterface;
+
+class VideoGame implements SchemaTypeInterface
+{
+    public function getJsonLd($context = true, $json_object = true)
+    {
+        $jsonLd = [
+            '@type' => 'VideoGame',
+        ];
+
+        if ($context === true) { $jsonLd['@context'] = 'http://schema.org'; }
+
+        $requiredFields = [];
+
+        foreach ($requiredFields as $field) {
+            if ($this->$field !== '') {
+                $jsonLd[$field] = $this->$field;
+            }
+        }
+
+        $recommendedFields = [];
+
+        foreach ($recommendedFields as $field) {
+            if ($this->$field !== '') {
+                $jsonLd[$field] = $this->$field;
+            }
+        }
+
+        $optionalFields = [];
+
+        foreach ($optionalFields as $field) {
+            if ($this->$field !== '') {
+                $jsonLd[$field] = $this->$field;
+            }
+        }
+
+        if ($json_object === true) {
+            $object = (object)$jsonLd;
+
+            return json_encode($object);
+        }
+
+        return $jsonLd;
+    }
+}
