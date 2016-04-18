65420dd95f470dde2cd9993dd2a05cbb328ae10a
David Craig <david.craig10@nhs.net>
2016-04-18 10:05:15 +0100

    Add optional fields.

--- src/Types/SchemaOrg/Thing/Intangible/GameServer.php
+++ src/Types/SchemaOrg/Thing/Intangible/GameServer.php
@@ -5,11 +5,44 @@ use Taskforcedev\StructuredData\Types\SchemaTypeInterface;
 
 class GameServer extends Thing implements SchemaTypeInterface
 {
+    public $game;
+    public $playersOnline;
+    public $serverStatus;
+
+    public function setGame($game)
+    {
+        $this->game = $game;
+    }
+
+    public function setPlayersOnline($playersOnline)
+    {
+        $this->playersOnline = $playersOnline;
+    }
+
+    public function setServerStatus($serverStatus)
+    {
+        $this->serverStatus = $serverStatus;
+    }
+
+
+
     public function getJsonLd($context = false, $json_object = false)
     {
         $jsonLd = [
             '@type' => 'GameServer',
         ];
+
+        $optionalFields = [
+            'game',
+            'playersOnline',
+            'serverStatus',
+        ];
+
+        foreach ($optionalFields as $field) {
+            if ($this->$field !== '') {
+                $jsonLd[$field] = $this->$field;
+            }
+        }
 
         if ($context === true) { $jsonLd['@context'] = 'http://schema.org'; }
 
