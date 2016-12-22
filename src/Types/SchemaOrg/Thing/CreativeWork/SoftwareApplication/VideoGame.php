da3c44cb9850b4bd0669c45cad56512295262670
PrivateSniper <privatesniper@gmail.com>
2016-12-22 12:21:47 +0000

    Seperate variables from logic.

--- src/Types/SchemaOrg/Thing/CreativeWork/SoftwareApplication/VideoGame.php
+++ src/Types/SchemaOrg/Thing/CreativeWork/SoftwareApplication/VideoGame.php
@@ -1,36 +1,43 @@
 <?php namespace Taskforcedev\StructuredData\Types\SchemaOrg\Thing\CreativeWork\SoftwareApplication;
 
+use Taskforcedev\StructuredData\Types\SchemaOrg\Thing;
 use Taskforcedev\StructuredData\Types\SchemaTypeInterface;
 
-class VideoGame implements SchemaTypeInterface
+class VideoGame extends Thing implements SchemaTypeInterface
 {
+    public $type = 'VideoGame';
+    public $requiredFields;
+    public $recommendedFields;
+    public $optionalFields;
+
+    public function __construct()
+    {
+        $this->requiredFields = [];
+        $this->recommendedFields = [];
+        $this->optionalFields = [];
+    }
+
     public function getJsonLd($context = true, $json_object = true)
     {
         $jsonLd = [
-            '@type' => 'VideoGame',
+            '@type' => $this->type,
         ];
 
         if ($context === true) { $jsonLd['@context'] = 'http://schema.org'; }
 
-        $requiredFields = [];
-
-        foreach ($requiredFields as $field) {
+        foreach ($this->requiredFields as $field) {
             if ($this->$field !== '') {
                 $jsonLd[$field] = $this->$field;
             }
         }
 
-        $recommendedFields = [];
-
-        foreach ($recommendedFields as $field) {
+        foreach ($this->recommendedFields as $field) {
             if ($this->$field !== '') {
                 $jsonLd[$field] = $this->$field;
             }
         }
 
-        $optionalFields = [];
-
-        foreach ($optionalFields as $field) {
+        foreach ($this->optionalFields as $field) {
             if ($this->$field !== '') {
                 $jsonLd[$field] = $this->$field;
             }
